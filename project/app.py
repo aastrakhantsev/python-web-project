8f004278fc368a503151c580c94e2a46e512e0f6
aastrakhantsev <astrakhantsev.aa@phystech.edu>
2022-05-23 16:29:40 +0300

    telebot replaced with aiogram, added async requests

--- project/app.py
+++ project/app.py
@@ -1,34 +1,43 @@
-import os
-import requests
-import telebot
 from bs4 import BeautifulSoup
 from portfolio import wal
+import aiohttp
+import aiogram
+import os
 
-bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
+bot = aiogram.Bot(os.getenv('BOT_TOKEN'))
+dispatcher = aiogram.dispatcher.Dispatcher(bot)
+parse_mode = aiogram.types.ParseMode.HTML
 
 
-def get_rate(ticker):
-    html = requests.get(f'https://finance.yahoo.com/quote/{ticker}-USD?p={ticker}-USD')
-    soup = BeautifulSoup(html.text, 'html.parser')
+async def get(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text(encoding='utf-8')
+
+
+async def get_rate(ticker):
+    url = f'https://finance.yahoo.com/quote/{ticker}-USD?p={ticker}-USD'
+    text = await get(url)
+    soup = BeautifulSoup(text, 'html.parser')
     result = str(soup.find('fin-streamer', attrs={'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).contents[0])
     return result
 
 
-@bot.message_handler(commands=['start', 'help'])
-def main(message):
+@dispatcher.message_handler(commands=['start', 'help'])
+async def main(message):
     text = (
-        '/help - to see this message \n'
-        '/guide - to know basics of interaction \n\n'
-        '/rate - core function \n'
-        '/add - add currency to your wallet\n'
-        '/remove - remove currency to your wallet\n'
-        '/wallet - get wallet info\n'
+        '<i>/help</i> - to see this message \n'
+        '<i>/guide</i> - to know basics of interaction \n\n'
+        '<i>/rate</i> - core function \n'
+        '<i>/add</i> - add currency to your wallet\n'
+        '<i>/remove</i> - remove currency to your wallet\n'
+        '<i>/wallet</i> - get wallet info\n'
     )
-    bot.send_message(message.chat.id, text)
+    await bot.send_message(message.chat.id, text, parse_mode=parse_mode)
 
 
-@bot.message_handler(commands=['guide'])
-def guide(message):
+@dispatcher.message_handler(commands=['guide'])
+async def guide(message):
     text = (
         '<i>/rate</i>\n'
         'Enter  <b>/rate + [ticker]</b>    to get rate of cryptocurrency\n\n'
@@ -39,54 +48,53 @@ def guide(message):
         '<i>/wallet</i>\n'
         'Enter  <b>/wallet</b>    to get your portfolio'
     )
-    bot.send_message(message.chat.id, text, parse_mode='html')
+    await bot.send_message(message.chat.id, text, parse_mode=parse_mode)
 
 
-@bot.message_handler(commands=['rate'])
-def rate(message):
+@dispatcher.message_handler(commands=['rate'])
+async def rate(message):
     ticker = (message.text.split(' ')[1])
-    val = get_rate(ticker)
+    val = await get_rate(ticker)
     text = f'One <b>{ticker}</b> is <b>${val}</b>'
-    bot.send_message(message.chat.id, text, parse_mode='html')
+    await bot.send_message(message.chat.id, text, parse_mode=parse_mode)
 
 
-@bot.message_handler(commands=['add'])
-def add(message):
+@dispatcher.message_handler(commands=['add'])
+async def add(message):
     ticker = (message.text.split(' ')[1])
     num = (message.text.split(' ')[2])
     if ticker in wal:
         wal[ticker] += float(num)
     else:
         wal[ticker] = float(num)
-    bot.send_message(message.chat.id, f'{num} {ticker} added to wallet', parse_mode='html')
+    await bot.send_message(message.chat.id, f'{num} {ticker} added to wallet', parse_mode=aiogram.types.ParseMode.HTML)
 
 
-@bot.message_handler(commands=['remove'])
-def remove(message):
+@dispatcher.message_handler(commands=['remove'])
+async def remove(message):
     ticker = (message.text.split(' ')[1])
-    num = (message.text.split(' ')[2])
-    if num == 'all':
-        num = wal[ticker]
+    val = (message.text.split(' ')[2])
+    if val == 'all':
+        val = wal[ticker]
     if ticker in wal:
-        wal[ticker] -= float(num)
+        wal[ticker] -= float(val)
         if wal[ticker] <= 0:
             wal.pop(ticker)
     else:
-        bot.send_message(message.chat.id, f'No {ticker} in your wallet', parse_mode='html')
-    bot.send_message(message.chat.id, f'{num} {ticker} removed from wallet', parse_mode='html')
+        await bot.send_message(message.chat.id, f'No {ticker} in your wallet', parse_mode=parse_mode)
+    await bot.send_message(message.chat.id, f'{val} {ticker} removed from your wallet', parse_mode=parse_mode)
 
 
-@bot.message_handler(commands=['wallet'])
-def wallet(message):
+@dispatcher.message_handler(commands=['wallet'])
+async def wallet(message):
     text = ''
     summary = 0
-    for item in wal.items():
-        key = item[0]
-        value = item[1]
+    for key, value in wal.items():
         text += f'{float(value)} of {key},  that is <i>${float(value) * float(get_rate(key))}</i>\n'
         summary += float(value) * float(get_rate(key))
     text += f'\nYour wallet is <i>${summary}</i>'
-    bot.send_message(message.chat.id, text, parse_mode='html')
+    await bot.send_message(message.chat.id, text, parse_mode=parse_mode)
 
 
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    aiogram.executor.start_polling(dispatcher)
